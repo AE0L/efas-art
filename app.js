cbb6f95af5ba303c2cd471254ea9046727c14f83
Aeol <carlj15.cj@gmail.com>
2021-11-23 22:06:05 +0800

    sample routes & template

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express()
 const port = 8080
 
 app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'templates'))
 app.use(express.static(path.join(__dirname, 'public')))
-
-app.get('/', (req, res) => res.sendFile('index'))
+app.use('/', require('./routes/base'))
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}/`)
---
+++ routes/base.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.render('index', {
+        msg: 'Hello, World!'
+    })
+})
+
+module.exports = router
