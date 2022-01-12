1e5002ebc5eb24670a8f58c965c23369eb34adc6
Aeol <carlj15.cj@gmail.com>
2022-01-12 15:04:58 +0800

    dashboard home

--- routes/dashboard/index.js
+++ routes/dashboard/index.js
@@ -19,6 +19,15 @@ router.get('/', load_user_dashboard, async (req, res) => {
 
     arts.sort((a, b) => new Date(b.creation_date) - new Date(a.creation_date))
 
+    let wats = []
+
+    wat_collections.forEach(async (wat_col) => {
+        const _watermarks = await wat_col.watermarks
+        wats.push(..._watermarks)
+    })
+
+    wats.sort((a, b) => new Date(b.creation_date) - new Date(a.creation_date))
+
     const date_now = moment()
     const date_week_before = moment().subtract(7, 'days')
 
@@ -27,21 +36,44 @@ router.get('/', load_user_dashboard, async (req, res) => {
         return art_mom.isBetween(date_week_before, date_now)
     })
 
+    rec_arts = rec_arts.map(a => ({
+        id: a.id,
+        title: a.name,
+        pic: a.document
+    }))
+
+    let rec_wats = wats.filter(wat => {
+        let wat_mom = moment(wat.creation_date)
+        return wat_mom.isBetween(date_week_before, date_now)
+    })
+
+    rec_wats = rec_wats.map(a => ({
+        id: a.id,
+        title: a.name,
+        pic: a.document
+    }))
+
+
     let rec_art_cols = art_collections.filter(art_col => {
         let art_col_mom = moment(art_col.creation_date)
         return art_col_mom.isBetween(date_week_before, date_now)
     })
 
-    let rec_wat_cols = wat_collections.filter(wat_col => {
-        let wat_col_mom = moment(wat_col.creation_date)
-        return wat_col_mom.isBetween(date_week_before, date_now)
+    rec_art_cols = rec_art_cols.map(async (ac) => {
+        const _arts = await ac.artworks
+
+        return {
+            id: ac.id,
+            title: ac.name,
+            pic: _arts[0].document
+        }
     })
 
     return res.render('dashboard_home.ejs', {
         user: { id: user.id },
         rec_arts: rec_arts,
+        rec_wats: rec_wats,
         rec_art_cols: rec_art_cols,
-        rec_wat_cols: rec_wat_cols
     })
 })
 
