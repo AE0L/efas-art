7a052c152f87cd2d2a3790aef0e5788915945520
joseph-tupaen <56032678+joseph-tupaen@users.noreply.github.com>
2022-01-28 12:28:41 +0800

    Font Edit Fix

--- public/js/editor-watermark.js
+++ public/js/editor-watermark.js
@@ -4,7 +4,7 @@ const gallery = document.getElementById('gallery');
 
 var deleteIcon = "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3C!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'%3E%3Csvg version='1.1' id='Ebene_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='595.275px' height='595.275px' viewBox='200 215 230 470' xml:space='preserve'%3E%3Ccircle style='fill:%23F44336;' cx='299.76' cy='439.067' r='218.516'/%3E%3Cg%3E%3Crect x='267.162' y='307.978' transform='matrix(0.7071 -0.7071 0.7071 0.7071 -222.6202 340.6915)' style='fill:white;' width='65.545' height='262.18'/%3E%3Crect x='266.988' y='308.153' transform='matrix(0.7071 0.7071 -0.7071 0.7071 398.3889 -83.3116)' style='fill:white;' width='65.544' height='262.179'/%3E%3C/g%3E%3C/svg%3E";
 var img = document.createElement('img');
-
+var addText = document.getElementById("addText")
 var activeObject = null;
 var textColor = document.getElementById("textColor");
 var objectType = null;
@@ -88,11 +88,6 @@ function renderIcon(ctx, left, top, styleOverride, fabricObject) {
     ctx.restore();
 }
 
-
-
-
-
-
 canvas.on('selection:created', function() {
     activeObject = canvas.getActiveObject();
     objectType = activeObject.get('type');
@@ -108,6 +103,23 @@ canvas.on('selection:created', function() {
         textUnderline.disabled = false;
         fontFamily.value = activeObject.fontFamily;
         fontSize.value = activeObject.fontSize;
+        fontSliderCounter.innerText = fontSize.value;
+
+        if (activeObject.fontWeight === 'bold')
+            textBold.checked = true;
+        else
+            textBold.checked = false;
+
+        if (activeObject.fontWeight === 'italic')
+            textItalic.checked = true;
+        else
+            textItalic.checked = false;
+
+        if (activeObject.underline === true)
+            textUnderline.checked = true;
+        else
+            textUnderline.checked = false;
+
     } else {
         textColor.disabled = true;
         fontFamily.disabled = true;
@@ -134,6 +146,21 @@ canvas.on('selection:updated', function(options) {
         fontFamily.value = activeObject.fontFamily;
         fontSize.value = activeObject.fontSize;
 
+        if (activeObject.fontWeight === 'bold')
+            textBold.checked = true;
+        else
+            textBold.checked = false;
+
+        if (activeObject.fontWeight === 'italic')
+            textItalic.checked = true;
+        else
+            textItalic.checked = false;
+
+        if (activeObject.underline === true)
+            textUnderline.checked = true;
+        else
+            textUnderline.checked = false;
+
     } else {
         textColor.disabled = true;
         fontFamily.disabled = true;
@@ -152,7 +179,10 @@ canvas.on('selection:cleared', function(options) {
     textBold.disabled = true;
     textItalic.disabled = true;
     textUnderline.disabled = true;
-
+    textBold.checked = false;
+    textItalic.checked = false;
+    textUnderline.checked = false;
+    fontSliderCounter.innerText = 0;
 });
 
 
@@ -176,19 +206,6 @@ isTransparent.addEventListener("input", function() {
     canvas.requestRenderAll();
 });
 
-textUnderline.addEventListener("input", function() {
-    activeObject = canvas.getActiveObject();
-    if (activeObject.underline === false) {
-        activeObject.underline = true
-        activeObject.set({ dirty: true });
-        canvas.requestRenderAll();
-    } else {
-        activeObject.underline = false
-        activeObject.set({ dirty: true });
-        canvas.requestRenderAll();
-    }
-
-});
 
 lineWeight.addEventListener("input", function() {
     activeObject = canvas.getActiveObject();
@@ -215,6 +232,7 @@ $("#toggleDraw").click(function() {
         $(this).removeClass('btn-secondary')
         $(this).text('Signature Mode: ON')
 
+        addText.disabled = true;
         canvas.isDrawingMode = true;
         lineWeight.disabled = false;
         lineColor.disabled = false;
@@ -223,6 +241,7 @@ $("#toggleDraw").click(function() {
         $(this).removeClass('btn-primary')
         $(this).text('Signature Mode: OFF')
 
+        addText.disabled = false;
         canvas.isDrawingMode = false;
         lineWeight.disabled = true;
         lineColor.disabled = true;
@@ -230,24 +249,21 @@ $("#toggleDraw").click(function() {
 });
 
 $("#addText").click(function() {
-    $('#toggleDraw').click()
-
+    
     canvas.add(new fabric.IText('Tap and Type', {
         fontFamily: 'arial',
         left: 100,
         top: 100,
     }));
 
-    canvas.setActiveObject(canvas.item(0));
-
+    var countObj = canvas.getObjects().length;
+    canvas.setActiveObject(canvas.item(countObj-1));
     canvas.isDrawingMode = false;
-
     lineWeight.disabled = true;
     lineColor.disabled = true;
-
-    textBold.checked = false
-    textItalic.checked = false
-    textUnderline.checked = false
+    textBold.checked = false;
+    textItalic.checked = false;
+    textUnderline.checked = false;
 });
 
 textColor.addEventListener("input", function() {
@@ -265,37 +281,28 @@ $('#fontFamily').on('change', function() {
 
 fontSize.addEventListener("input", function() {
     activeObject = canvas.getActiveObject();
-    sliderValue = $("#fontSize").val();
-    fontSliderCounter.innerText = sliderValue
-    activeObject.fontSize = sliderValue;
+    activeObject.fontSize = fontSize.value;
     canvas.requestRenderAll();
 });
 
 textBold.addEventListener("input", function() {
-    if (this.checked) {
-        dtEditText('bold');
-    }
+    dtEditText('bold');  
 });
 
 textItalic.addEventListener("input", function() {
-    if (this.checked) {
-        dtEditText('italic');
-    }
+    dtEditText('italic');  
 });
 
 textUnderline.addEventListener("input", function() {
-    if (this.checked) {
-        activeObject = canvas.getActiveObject();
-        if (activeObject.underline === false) {
-            activeObject.underline = true
-            activeObject.set({ dirty: true });
-            canvas.requestRenderAll();
-        } else {
-            activeObject.underline = false
-            activeObject.set({ dirty: true });
-            canvas.requestRenderAll();
-        }
-
+    activeObject = canvas.getActiveObject();
+    if (activeObject.underline === false) {
+        activeObject.underline = true
+        activeObject.set({ dirty: true });
+        canvas.requestRenderAll();
+    } else {
+        activeObject.underline = false
+        activeObject.set({ dirty: true });
+        canvas.requestRenderAll();
     }
 });
 
